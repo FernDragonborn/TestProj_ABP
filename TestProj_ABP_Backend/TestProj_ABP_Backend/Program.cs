b690ce43c750e74ac4454549f22f79b5b3d44e00
FernDragonborn <FernDragonborn@gmail.com>
2024-02-22 13:06:34 +0200

    Projects created

---
+++ TestProj_ABP_Backend/TestProj_ABP_Backend/Program.cs
@@ -0,0 +1,15 @@
+namespace TestProj_ABP_Backend
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+            var app = builder.Build();
+
+            app.MapGet("/", () => "Hello World!");
+
+            app.Run();
+        }
+    }
+}
