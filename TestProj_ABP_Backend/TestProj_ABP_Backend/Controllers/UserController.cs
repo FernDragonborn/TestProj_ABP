b353b14da6e4753a2ff198e8a2c18508c54e07dc
FernDragonborn <FernDragonborn@gmail.com>
2024-02-24 14:51:08 +0200

    Some adds into controller + random str generator + start of fingerprint

--- TestProj_ABP_Backend/TestProj_ABP_Backend/Controllers/UserController.cs
+++ TestProj_ABP_Backend/TestProj_ABP_Backend/Controllers/UserController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using TestProj_ABP_Backend.Services;
 
 namespace TestProj_ABP_Backend.Controllers;
 
@@ -9,15 +10,19 @@ public class UserController : ControllerBase
     /// <summary>
     /// Get
     /// </summary>
-    /// <param name="id"></param>
+    /// <param name="deviceToken"></param>
     /// <returns>HTTP responce.</returns>
     /// <response code="200">Succesfully founded</response>
-    [HttpGet("button-color")]
     [ProducesResponseType(200)]
-    public IActionResult ButtonColor([FromQuery] string id)
+    [HttpGet("button-color")]
+    public IActionResult ButtonColor([FromQuery(Name = "device-token")] string? DeviceToken)
     {
-        return Ok(new[] { "value1", "value2" });
+        if (!string.IsNullOrWhiteSpace(DeviceToken))
+        {
+            return Ok(new[] { "value1", "value2" });
+        }
+        DeviceToken.Trim();
+        return Ok(UserService.GenerateRandomString(6));
     }
-
 }
 
---
+++ TestProj_ABP_Backend/TestProj_ABP_Backend/Services/UserService.cs
@@ -0,0 +1,38 @@
+﻿using System.Text;
+
+namespace TestProj_ABP_Backend.Services
+{
+    public class UserService
+    {
+        private static readonly Random random = new Random();
+
+        /// <summary>
+        /// Generates random string from numbers, latin lower and upper case letters of needed lenght.
+        /// </summary>
+        /// <param name="length">Lenght of string needed.</param>
+        /// <returns>Random string.</returns>
+        public static string GenerateRandomString(int length)
+        {
+            const string chars = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+            StringBuilder stringBuilder = new StringBuilder(length);
+
+            for (int i = 0; i < length; i++)
+            {
+                int randomIndex = random.Next(chars.Length);
+                char randomChar = chars[randomIndex];
+                stringBuilder.Append(randomChar);
+            }
+
+            return stringBuilder.ToString();
+        }
+
+
+        public string?[] GetUserCredentials(IHttpContextAccessor httpContextAccessor)
+        {
+            var userCredentials = new string?[2];
+            userCredentials[0] = httpContextAccessor.HttpContext?.Request.Headers.UserAgent;
+            userCredentials[1] = httpContextAccessor.HttpContext?.Connection.RemoteIpAddress?.ToString();
+            return userCredentials;
+        }
+    }
+}
